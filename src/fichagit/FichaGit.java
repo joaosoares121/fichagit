75ea33aca57ca9db12d8b25d6fafd61c90b9d674
joaosoares <joaosoares@mbp-de-joao>
2019-05-23 23:58:02 +0100

    Quinto Commit Mas com a reversão para o Segundo

--- src/fichagit/FichaGit.java
+++ src/fichagit/FichaGit.java
@@ -15,7 +15,7 @@ public class FichaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World Quarto");
+        System.out.println("Hello World commit dois");
     }
     
 }
